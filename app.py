099b133802dc1ae9ccec2f6cc3b1a607c6453edd
annaweeks <aoweeks@yahoo.com>
2022-01-31 22:08:06 -0800

    adding query comments

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import time
 import os
 from flask_sqlalchemy import SQLAlchemy
 from models import create_classes
+import datetime
 
 
 
@@ -68,31 +69,34 @@ def index():
         Newdf_daily['Datetime'] = pd.to_datetime(Newdf_daily['time'],unit = 's')
         Newdf_daily['Year'] = pd.to_datetime(Newdf_daily['Datetime'],errors = 'ignore').dt.year
         # add to db
-        Newdf_daily.to_sql("crypto_price", con = engine, if_exists='replace', index=False)
+        Newdf_daily.to_sql("crypto_price", con = engine, if_exists='append', index=False)
+    
+    most_recent_timestamp_in_db = db.session.query(CryptoCurr.time).\
+        order_by(CryptoCurr.time.desc()).\
+        limit(1).all()[0][0]
+    current_date = time.time()
 
-    oldest_timestamp_in_db = db.session.query(CryptoCurr.time).limit(1).all()[0][0]
-    current_time = int(time.time())
+    limit = current_date - most_recent_timestamp_in_db
+    days = int(limit/60/60/24)
 
-    while current_time > oldest_timestamp_in_db:
-        # api call to get more data
-        oldest_timestamp_in_db -= 1
-        url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=2000&toTs=-1&api_key={api_key}"
+    # while current_time > most_recent_timestamp_in_db:
+    
+    # api call to get more data
+    if days > 0:
+        url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit={days}&toTs=-1&api_key={api_key}"
         r = requests.get(url)
         data = r.json()
-        oldest_timestamp_in_db = db.session.query(CryptoCurr.time).limit(1).all()
-
+  
         price_df = pd.DataFrame(data['Data']['Data'])
-        df_daily = price_df.append(df_daily)
-        
-    # cleaning df
-    df_daily = df_daily[df_daily['time'] >= 1388563200]
-    Newdf_daily = df_daily[['time','high','low','open','volumefrom','volumeto','close','conversionType']].copy()
-    Newdf_daily['Datetime'] = pd.to_datetime(Newdf_daily['time'],unit = 's')
-    Newdf_daily['Year'] = pd.to_datetime(Newdf_daily['Datetime'],errors = 'ignore').dt.year
 
-    # load df into db
-    # Newdf_daily.to_sql(name='crypto_daily_table', con=engine, if_exists='append', index=False) 
-    Newdf_daily.to_sql("crypto_price", con = engine, if_exists='replace')
+        # cleaning df
+        Newdf_daily = price_df[['time','high','low','open','volumefrom','volumeto','close','conversionType']].copy()
+        Newdf_daily['Datetime'] = pd.to_datetime(Newdf_daily['time'],unit = 's')
+        Newdf_daily['Year'] = pd.to_datetime(Newdf_daily['Datetime'],errors = 'ignore').dt.year
+
+        # load df into db
+        # Newdf_daily.to_sql(name='crypto_daily_table', con=engine, if_exists='append', index=False) 
+        Newdf_daily.to_sql("crypto_price", con = engine, if_exists='append')
 
     return render_template("index.html")
  
