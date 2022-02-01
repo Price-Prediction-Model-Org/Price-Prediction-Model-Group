8cb0e3e2f361f38349a4bc4244f31240c8fcf410
annaweeks <aoweeks@yahoo.com>
2022-02-01 00:08:37 -0800

    still trying to fix while loop

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def index():
 
             # API call
             oldest_timestamp_in_df -= 1
-            url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=2000&toTs={oldest_timestamp_in_db}&api_key={api_key}"
+            url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=2000&toTs={oldest_timestamp_in_df}&api_key={api_key}"
             r = requests.get(url)
             data = r.json()
             df_daily = pd.DataFrame(data['Data']['Data'])
