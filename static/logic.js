552803e4ec3c0267f03f7fddd71fe9b5e10c106a
annaweeks <aoweeks@yahoo.com>
2022-01-31 17:30:21 -0800

    adding files for heroku

--- static/logic.js
+++ static/logic.js
@@ -1,14 +1,11 @@
 firstFive = d3.json("/first_five")
-latestData = d3.json("/get_latest_data")
 
 // Function initializes the dashboard.
 function init() {
 
     console.log('testing')
 
-    latestData.then((data) => {
-        console.log(data)
-    });
+    
     
     firstFive.then((data) => {
         console.log(data)
